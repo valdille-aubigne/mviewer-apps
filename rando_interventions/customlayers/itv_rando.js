b835fcf96c33dcfb7ec9df188c02ac243f10582e
pbarille <pascal.barille@valdille-aubigne.fr>
2019-10-14 15:45:44 +0200

    intégration des custom au dossier apps

---
+++ rando_interventions/customlayers/itv_rando.js
@@ -0,0 +1,8 @@
+{
+mviewer.customLayers.itv_rando = {};
+
+mviewer.customLayers.itv_rando.layer = new ol.layer.Vector({
+    source: new ol.source.Vector()    
+});
+mviewer.customLayers.itv_rando.handle = false;
+}

1e365815914b8e2fc3624333cd0f0af4d2fefaa0
Pascal <pascal.barille@valdille-aubigne.fr>
2020-12-04 16:01:56 +0100

    update

--- faune_flore_demo/customlayers/obs_faune_flore_demo.js
+++
@@ -1,8 +0,0 @@
-{
-mviewer.customLayers.obs_faune_flore_demo = {};
-
-mviewer.customLayers.obs_faune_flore_demo.layer = new ol.layer.Vector({
-    source: new ol.source.Vector()    
-});
-mviewer.customLayers.obs_faune_flore_demo.handle = false;
-}
